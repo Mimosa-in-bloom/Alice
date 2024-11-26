674bb7fd84b0231de405e1afa1d963f7bb599082
Mimosa-in-bloom <yutudrug@gmail.com>
2024-11-26 08:13:26 -0500

    sad

--- winter.py
+++ winter.py
@@ -1,2 +1,9 @@
+from guizero import App, Text, Picture
+app = App(title = 'K')
+text = Text(app, font= "Arial", text="Make AI helpful for everyone")
+text1 = Text(app, font= "Arial", text="hjhhvi b ghiuyh iv hvhhv98u98vbbvv")
+pic =  Picture(app, image="cat.png", width= 390, height= 520)
+text2 = Text(app, font= "Arial",text="hjhhvi b ghiuyh iv hvhhv98u98vbbvv")
+text3 = Text(app, font= "Arial",text="hjhhvi b ghiuyh iv hvhhv98u98vbbvv")
 
-print("Hello")
+app.display()
